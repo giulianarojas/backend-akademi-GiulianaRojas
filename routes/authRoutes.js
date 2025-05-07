ea7d41c15183ac12971de33f5ed0ade4591fda8c
giulianarojas <giulianaroj5@gmail.com>
2025-05-07 15:20:02 -0300

    Reubique las validaciones en un archivo separado (validations.js) para mantener el código más ordenado y reutilizable. Se actualizó authRoutes para importar desde ahí en lugar de definir las validaciones directamente.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { register } = require('../controllers/authController');
-const { userValidator } = require('../util/validators/user-validator');
+const { register, login } = require('../controllers/authController');
+
+//importo validadores 
+const { userValidator, loginValidator, passwordValidator, emailValidator} = require ('../utils/validators/validations')
 
 const { auth, checkRole } = require('../middleware/auth');
-const { body } = require('express-validator');
 
 
-//validador para autenticacion
-
-//para usuario
-const userValidator = [
-    body('nombre').trim().not().isEmpty().withMessage('El nombre es obligatorio'),
-    body('email').normalizeEmail().isEmail().withMessage('Ingresa un email valido'),
-    body('password').isLength({min: 6}).withMessage('La contraseña debe tener al menos 6 caracteres'),
-    body('rol').isIn(['admin', 'recepcion']).withMessage('El rol debe ser admin o recepcion')
-];
-
-//login
-
-const loginValidator = [
-    body('email').normalizeEmail().withMessage('Ingresa un email valido'),
-    body('password').not().isEmpty().withMessage('La contraseña es obligatoria')
-];
 
 
-const passwordValidator = [
-    body('password').isLength({min: 6}).withMessage('La contraseña debe tener al menos 6 caracteres')
-];
 
 
 //rutas publicas q no requieren autenticarse
@@ -37,9 +19,8 @@ router.post('/login', loginValidator, login);
 
 //recuperar contraseña
 
-router.post('/forgot-password',
-    body('email').normalizeEmail().isEmail().withMessage('ingresa un email valido'), forgotPassword
-);
+router.post('/forgot-password', emailValidator, forgotPassword);
+
 
 router.get('/reset-password/:token', verifyResetToken);
 router.post('/reset-password/:token', passwordValidator, resetPassword);
--- utils/validators/validations.js
+++ utils/validators/validations.js
@@ -1,10 +1,32 @@
 const { body } = require('express-validator');
 
+//validacion para usuario
+
 const userValidator = [
   body('email').isEmail().withMessage('Email inválido'),
   body('password').isLength({ min: 6 }).withMessage('Min 6 caracteres'),
   body('nombre').not().isEmpty().withMessage('Nombre requerido'),
-  body('rol').isIn(['admin', 'recepcion']).withMessage('Rol inválido')
+  body('rol').isIn(['admin', 'recepcion']).withMessage('Rol invalido')
 ];
 
-module.exports = { userValidator };
+//para login
+
+const loginValidator = [
+  body('email').normalizeEmail().withMessage('Ingresa un email valido'),
+  body('password').not().isEmpty().withMessage('La contraseña es obligatoria')
+];
+
+//para email
+const emailValidator = [
+  body('email').normalizeEmail().isEmail().withMessage('Debes ingresar un email valido')
+];
+
+//para cambio de contrseña
+const passwordValidator = [
+  body('password').isLength({min: 6}).withMessage('La contraseña debe tener al menos 6 caracteres')
+];
+
+
+
+module.exports = { userValidator,loginValidator,
+  passwordValidator, emailValidator };
